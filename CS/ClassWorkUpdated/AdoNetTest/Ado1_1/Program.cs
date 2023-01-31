990eb636c0c5df90b0d3aa6e3e3f821ebbd6e679
Somnovem <artem.zhmura606@gmail.com>
2023-01-31 21:16:43 +0200

    31.01

---
+++ CS/ClassWorkUpdated/AdoNetTest/Ado1_1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data Source=localhost
+            //Data Source=192.168.1.1
+            //Data Source=192.168.1.1\\SQLEXPRESS
+            //Data Source=(local)\\SQLEXPRESS
+            //Data Source=(local)
+            //Data Source=192.168.1.1\\MSSQL
+            string connectionStr = "Data Source = DESKTOP-6FEP48M\\SQLEXPRESS;Initial Catalog = TeachersDb;Integrated Security=true"; //true|yes|sspi
+            //string connectionStr = "Data Source = DESKTOP-6FEP48M\\SQLEXPRESS;Initial Catalog = TeachersDb;User ID = Mark; Password=1234";
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                //SqlCommand cmd = conn.CreateCommand();
+                //cmd.CommandText = "select * from Students";
+                ShowTable("Faculties", conn);
+                ShowScalarValue("Faculties", conn);
+                //InsertFaculty(conn);
+                //Console.WriteLine("----------------------------------------------");
+                //ShowTable("Faculties", conn);
+                //DeleteFaculty(conn);
+                //Console.WriteLine("----------------------------------------------");
+                //ShowTable("Faculties", conn);
+            }
+            Console.WriteLine("\nPress enter key...");
+            Console.ReadLine();
+        }
+
+        private static void InsertFaculty(SqlConnection conn)
+        {
+            Console.Write("\nEnter name: ");
+            string name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name)){
+                Console.WriteLine("Invalid name");
+                return;
+            }
+            //SqlParameter param = new SqlParameter("@name",SqlDbType.NVarChar,100);
+           // param.Value = name;
+            string sql = $"insert into Faculties values(@name)";
+            SqlCommand command = new SqlCommand(sql, conn);
+            //command.Parameters.Add(param);
+            command.Parameters.Add(new SqlParameter("@name", name));
+            int res = command.ExecuteNonQuery();
+            Console.WriteLine($"{res} row(s) affected");
+        }
+
+        static void ShowTable(string tableName, SqlConnection conn) {
+            string sql = "select * from " + tableName;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                bool first = true;
+                while (reader.Read())
+                {
+                    //reader["id"] - доступ к значению по имени колонки
+                    if (first)
+                    {
+                        first = false;
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader.GetName(i) + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                    for (int i = 0; i < reader.FieldCount; ++i)
+                    {
+                        Console.Write(reader[i] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void ShowScalarValue(string tableName, SqlConnection conn) {
+            string sql = "select count(*) from " + tableName;
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            int result;
+            object res = cmd.ExecuteScalar();
+            if (int.TryParse(res.ToString(), out result)){
+                Console.WriteLine($"Record count = {result}");
+            }
+            else
+            {
+                Console.WriteLine("Cannor exec query!");
+            }
+        }
+
+        private static void DeleteFaculty(SqlConnection conn) {
+            Console.Write("\nEnter Id: ");
+            int id;
+            try
+            {
+                 id= Convert.ToInt32(Console.ReadLine());
+            }
+            catch {
+                Console.WriteLine("Invalid ID");
+                return;
+            }
+            string sql = "delete from Faculties where ID = @id";
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.Parameters.Add(new SqlParameter("@id", id));
+            int res = command.ExecuteNonQuery();
+            Console.WriteLine($"{res} row(s) affected");
+        }
+    }
+}
---
+++ CS/ClassWorkUpdated/AdoNetTest/Pract/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStr = "Data Source = DESKTOP-6FEP48M\\SQLEXPRESS;Initial Catalog = StudentsGrades;Integrated Security=true";
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1-Connect to database");
+                Console.WriteLine("2-Disconnect from database");
+                Console.Write("Option: ");
+                int ans = 0;
+                try
+                {
+                    ans = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid option");
+                    Environment.Exit(1);
+                }
+                using (SqlConnection conn = new SqlConnection(connectionStr))
+                {
+                    Console.Clear();
+                    switch (ans)
+                    {
+                        case 1:
+                            if (conn.State == ConnectionState.Closed)
+                            {
+                                try
+                                {
+                                    conn.Open();
+                                    Console.WriteLine("Succssfully connected!");
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Could not connect to database!");
+                                    Console.ReadLine();
+                                }
+                            }
+                            break;
+                        case 2:
+                            if (conn.State == ConnectionState.Open)
+                            {
+                                try
+                                {
+                                    conn.Close();
+                                    Console.WriteLine("Succssfully disconnected!");
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("Could not disconnect from database!");
+                                    Console.ReadLine();
+                                }
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option!");
+                            break;
+                    }
+                    Console.ReadLine();
+                    break;
+                }
+            }
+            using (SqlConnection connect = new SqlConnection(connectionStr))
+            {
+                Console.ReadLine();
+            }
+        }
+        static void ShowTable(SqlConnection conn)
+        {
+            Console.Clear();
+            string sql = "select * from Grades";
+
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                bool first = true;
+                while (reader.Read())
+                {
+                    if (first)
+                    {
+                        first = false;
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader.GetName(i) + "\t");
+                        }
+                        Console.WriteLine();
+                    }
+                    for (int i = 0; i < reader.FieldCount; ++i)
+                    {
+                        Console.Write(reader[i] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+        static void ShowNames(SqlConnection conn)
+        {
+            Console.Clear();
+            string sql = "select Lastname + ' ' + Firstname + ' ' + Fathersname as 'Fullname'  from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+        static void ShowAverages(SqlConnection conn)
+        {
+            Console.Clear();
+            string sql = "select AvgGrade  from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+        static void ShowStudentsWithGradesHigher(SqlConnection conn,int grade)
+        {
+            Console.Clear();
+            string sql = "select Lastname + ' ' + Firstname + ' ' + Fathersname as 'Fullname'  from Grades where AvgGrade > @grade";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.Add(new SqlParameter("@grade", grade));
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+        static void ShowSubjectWithMinimalGrades(SqlConnection conn){
+            Console.Clear();
+            string sql = "select distinct SubjectMinimalAvgGrade from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                }
+            }
+        }
+        static void MinimalAvgGrade(SqlConnection conn) {
+            Console.Clear();
+            int res = 0;
+            string sql = "select Min(AvgGrade) from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            object result = cmd.ExecuteScalar();
+            if (int.TryParse(res.ToString(), out res))
+            {
+                Console.WriteLine($"Minimal avg grade = {res}");
+            }
+            else
+            {
+                Console.WriteLine("Cannor exec query!");
+            }
+        }
+        static void MaximalAvgGrade(SqlConnection conn)
+        {
+            Console.Clear();
+            int res = 0;
+            string sql = "select Max(AvgGrade) from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            object result = cmd.ExecuteScalar();
+            if (int.TryParse(res.ToString(), out res))
+            {
+                Console.WriteLine($"Maximal avg grade = {res}");
+            }
+            else
+            {
+                Console.WriteLine("Cannor exec query!");
+            }
+        }
+        static void NumOfStudentsMinMath(SqlConnection conn) {
+            Console.Clear();
+            int res = 0;
+            string sql = "select count(Lastname) from Grades";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+        }
+    }
+}
